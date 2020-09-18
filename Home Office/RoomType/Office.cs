20f24975326e57ceaa4a2cf8425f34c248e3fcad
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-09-18 10:16:51 -0400

    Updated Office to use ConfigFile contents

--- Home Office/RoomType/Office.cs
+++ Home Office/RoomType/Office.cs
@@ -11,8 +11,6 @@ namespace Home_Office.RoomType
 {
     public class Office : Room
     {
-        private bool _forceExternalGw = false;
-
         private RFExGateway _gw;
         private HdMd4x24kE _sw;
         private Tst902 _tp;
@@ -36,20 +34,20 @@ namespace Home_Office.RoomType
 
         private void InitializeGateways()
         {
-            if (_sys.SupportsInternalRFGateway && !_forceExternalGw)
+            if (_sys.SupportsInternalRFGateway && !_cfg.GetBool("panel", "external_gw", false))
             {
-                Log("InitializeGateways", "Using internal RF gateway");
+                Log("Office::InitializeGateways", "Using internal RF gateway");
 
                 _gw = _sys.ControllerRFGatewayDevice as RFExGateway;
             }
             else
             {
-                Log("InitializeGateways", "Creating external RF gateway");
+                Log("Office::InitializeGateways", "Creating external RF gateway");
 
-                _gw = new CenRfgwEx(0x0f, _sys);
+                _gw = new CenRfgwEx(_cfg.GetInteger("gateway", "ip_id", 0x0f), _sys);
 
                 if (_gw.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
-                    Log("InitializeGateways", "Failed to register {0}: {1}", _gw.Name, _gw.RegistrationFailureReason.ToString());
+                    Log("Office::InitializeGateways", "Failed to register {0}: {1}", _gw.Name, _gw.RegistrationFailureReason.ToString());
             }
         }
 
@@ -59,29 +57,30 @@ namespace Home_Office.RoomType
 
             if (_gw.Registered)
             {
-                Log("InitializeUI", "Creating TST-902 on {0}", _gw.Name);
-                _tp = new Tst902(0x03, _gw);
+                Log("Office::InitializeUI", "Creating TST-902 on {0}", _gw.Name);
+                _tp = new Tst902(_cfg.GetInteger("panel", "rf_id", 0x03), _gw);
                 _panels.Add_902(_tp);
             }
 
-            Log("InitializeUI", "Creating XPanel");
-            _xpanel = new XpanelForSmartGraphics(0x04, _sys);
+            Log("Office::InitializeUI", "Creating XPanel");
+            _xpanel = new XpanelForSmartGraphics(_cfg.GetInteger("xpanel", "ip_id", 0x04), _sys);
             _panels.Add(_xpanel);
 
-            Log("InitializeUI", "Registering devices");
+            Log("Office::InitializeUI", "Registering devices");
             _panels.Register();
 
         }
 
         private void InitializeDM()
         {
-            Log("InitializeDM", "Creating DM switch");
+            Log("Office::InitializeDM", "Creating DM switch");
 
-            _sw = new HdMd4x24kE(0x10, "192.168.1.122", _sys);
-            _sw.DMInputChange += _sw_DMInputChange;
+            _sw = new HdMd4x24kE(_cfg.GetInteger("switcher", "ip_id", 0x10),
+                                 _cfg.GetString("switcher", "ip", "192.168.1.10"),
+                                 _sys);
 
             if (_sw.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
-                Log("InitializeDM", "Failed to register {0}: {1}", _sw.Name, _sw.RegistrationFailureReason.ToString());
+                Log("Office::InitializeDM", "Failed to register {0}: {1}", _sw.Name, _sw.RegistrationFailureReason.ToString());
         }
 
         public override void Press(BasicTriList dev, uint join)
@@ -118,33 +117,6 @@ namespace Home_Office.RoomType
 
                     break;
             }
-        }
-
-        void _sw_DMInputChange(Switch dev, DMInputEventArgs args)
-        {
-            switch (args.EventId)
-            {
-                case DMInputEventIds.InputNameEventId:
-                    Log("_sw_DMInputChange", "Input {0} name: {1}", args.Number.ToString(), _sw.HdmiInputs[args.Number].NameFeedback.StringValue);
-                    break;
-                case DMInputEventIds.SourceSyncEventId:
-                    if (_sw.HdmiInputs[args.Number].VideoDetectedFeedback.BoolValue)
-                        _sw_ReportVideoAttributes(args.Number);
-                    else
-                        Log("_sw_DMInputChange", "Input {0} sync: None", args.Number.ToString());
-                    break;
-                default:
-                    Log("_sw_DMInputChange", "EventId={0}, Number={1}, Stream={2}", args.EventId.ToString(), args.Number.ToString(), args.Stream.ToString());
-                    break;
-            }
-        }
-
-        void _sw_ReportVideoAttributes(uint number)
-        {
-            Log("_sw_ReportVideoAttributes", String.Format("Input {0} sync: {1}x{2}@{3}", number,
-                _sw.HdmiInputs[number].HdmiInputPort.VideoAttributes.HorizontalResolutionFeedback.UShortValue,
-                _sw.HdmiInputs[number].HdmiInputPort.VideoAttributes.VerticalResolutionFeedback.UShortValue,
-                _sw.HdmiInputs[number].HdmiInputPort.VideoAttributes.FramesPerSecondFeedback.UShortValue));
         }
     }
 }
