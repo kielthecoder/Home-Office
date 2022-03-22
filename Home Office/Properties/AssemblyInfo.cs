4fe08d320c052f5133bd011ba3c1897d2e1704a1
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2022-03-22 11:11:18 -0400

    removed previous project

--- Home Office/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Home_Office")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Home_Office")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
